c5506b57fee896b809361208482e9199bd8d7c76
Michael Justin Donlon <your.email@example.com>
2024-11-07 10:17:54 +0100

    Clean rebuild with updated configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['justindonlon.com'],
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig 
